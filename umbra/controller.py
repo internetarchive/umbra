bf3c28f0b1a022cb0e0de4b177f86e7aa18afc75
Neil Minton <neil@archive.org>
2017-08-02 10:52:24 -0700

    Post outlinks to queue.

--- umbra/controller.py
+++ umbra/controller.py
@@ -229,16 +229,36 @@ class AmqpBrowserController:
                                                      self._producer.publish)
                 publish(payload, exchange=self._exchange, routing_key=client_id)
 
+        def post_outlinks(outlinks=None):
+            for link in outlinks:
+                 #  Each of these payload fields are required by AMQPUrlReceiver.java
+                 #+ in Heritrix.
+                 payload = {
+                    'url': link,
+                    'headers': '',
+                    'parentUrl': url,
+                    'parentUrlMetadata': parent_url_metadata,
+                    'method': 'GET',
+                 }
+                 self.logger.debug(
+                            'sending to amqp exchange=%s routing_key=%s payload=%s',
+                            self.exchange_name, client_id, payload)
+                 with self._producer_lock:
+                     publish = self._producer_conn.ensure(self._producer,
+                                                          self._producer.publish)
+                     publish(payload, exchange=self._exchange, routing_key=client_id)
+
         def browse_page_sync():
             self.logger.info(
                     'browser=%s client_id=%s url=%s behavior_parameters=%s',
                     browser, client_id, url, behavior_parameters)
             try:
                 browser.start()
-                browser.browse_page(
+                final_page_url, outlinks = browser.browse_page(
                         url, on_response=on_response,
                         behavior_parameters=behavior_parameters,
                         username=username, password=password)
+                post_outlinks(outlinks)
                 message.ack()
             except brozzler.ShutdownRequested as e:
                 self.logger.info("browsing did not complete normally, requeuing url {} - {}".format(url, e))
