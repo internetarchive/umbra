f31dc22b0c79b456b3583626771b9ca959697274
Misty De Méo <misty@archive.org>
2025-03-04 16:04:48 -0800

    chore: use importlib for version

--- umbra/__init__.py
+++ umbra/__init__.py
@@ -1,5 +1,5 @@
 from brozzler.browser import Browser
 from umbra.controller import AmqpBrowserController
-from pkg_resources import get_distribution as _get_distribution
-__version__ = _get_distribution('umbra').version
+from importlib.metadata import version as _version
+__version__ = _version('umbra')
 Umbra = AmqpBrowserController
