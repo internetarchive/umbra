4181d452567a6d31f0c73ce8748ca8f5e6f839d5
Misty De Méo <misty@archive.org>
2025-07-30 15:43:31 -0700

    fix all ruff check warnings

--- umbra/__init__.py
+++ umbra/__init__.py
@@ -1,4 +1,4 @@
-from brozzler.browser import Browser
+from brozzler.browser import Browser  # noqa F401
 from umbra.controller import AmqpBrowserController
 from importlib.metadata import version as _version
 __version__ = _version('umbra')
