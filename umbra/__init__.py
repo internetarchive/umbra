998b4fdf4cfeee9a0426d7829d5bc73506d9e681
Misty De Méo <misty@archive.org>
2025-07-30 15:06:33 -0700

    feat: add optional sentry support

--- umbra/__init__.py
+++ umbra/__init__.py
@@ -3,3 +3,20 @@ from umbra.controller import AmqpBrowserController
 from importlib.metadata import version as _version
 __version__ = _version('umbra')
 Umbra = AmqpBrowserController
+
+try:
+    import os
+    import sentry_sdk
+
+    DEPLOYMENT_ENVIRONMENT = os.environ.get("DEPLOYMENT_ENVIRONMENT", "DEV")
+    SENTRY_DSN = os.environ.get("SENTRY_DSN")
+
+    if SENTRY_DSN:
+        sentry_sdk.init(
+            dsn=SENTRY_DSN,
+            environment=DEPLOYMENT_ENVIRONMENT,
+            release=__version__,
+            traces_sample_rate=float(os.environ.get("SENTRY_TRACES_SAMPLE_RATE", "0")),
+        )
+except ImportError:
+    pass
