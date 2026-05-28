9869d046ed405bde000c992e70c2dc3683f5ffe9
Alex Dempsey <avdempsey@archive.org>
2026-05-28 10:40:25 -0700

    ruff format pass

--- umbra/__init__.py
+++ umbra/__init__.py
@@ -1,7 +1,8 @@
 from brozzler.browser import Browser  # noqa F401
 from umbra.controller import AmqpBrowserController
 from importlib.metadata import version as _version
-__version__ = _version('umbra')
+
+__version__ = _version("umbra")
 Umbra = AmqpBrowserController
 
 try:
